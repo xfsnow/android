9a3d35f02941325cd474a53803940fbcb7eb2eb4
xfsnow <xuefeng@snowpeak.org>
2017-09-14 18:17:12 +0800

    调通弹窗登录

--- CognitoAmazon/app/src/main/java/org/snowpeak/cognitoamazon/MainActivity.java
+++ CognitoAmazon/app/src/main/java/org/snowpeak/cognitoamazon/MainActivity.java
@@ -2,12 +2,118 @@ package org.snowpeak.cognitoamazon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.amazon.identity.auth.device.AuthError;
+import com.amazon.identity.auth.device.api.Listener;
+import com.amazon.identity.auth.device.api.authorization.AuthCancellation;
+import com.amazon.identity.auth.device.api.authorization.AuthorizationManager;
+import com.amazon.identity.auth.device.api.authorization.AuthorizeListener;
+import com.amazon.identity.auth.device.api.authorization.AuthorizeRequest;
+import com.amazon.identity.auth.device.api.authorization.AuthorizeResult;
+import com.amazon.identity.auth.device.api.authorization.ProfileScope;
+import com.amazon.identity.auth.device.api.authorization.User;
+import com.amazon.identity.auth.device.api.workflow.RequestContext;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+
+    private RequestContext requestContext;
+    private View mLoginButton;
+
+    private void fetchUserProfile() {
+        User.fetch(this, new Listener<User, AuthError>() {
+
+            /* fetch completed successfully. */
+            @Override
+            public void onSuccess(User user) {
+                final String name = user.getUserName();
+                final String email = user.getUserEmail();
+                final String account = user.getUserId();
+                final String zipcode = user.getUserPostalCode();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        updateProfileData(name, email, account, zipcode);
+
+                    }
+                });
+            }
+
+            /* There was an error during the attempt to get the profile. */
+            @Override
+            public void onError(AuthError ae) {
+     /* Retry or inform the user of the error */
+            }
+        });
+    }
+
+    private void updateProfileData(String name, String email, String account, String zipCode) {
+        StringBuilder profileBuilder = new StringBuilder();
+        profileBuilder.append(String.format("Welcome, %s!\n", name));
+        profileBuilder.append(String.format("Your email is %s\n", email));
+        profileBuilder.append(String.format("Your zipCode is %s\n", zipCode));
+        final String profile = profileBuilder.toString();
+        Log.d(TAG, "Profile Response: " + profile);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestContext = RequestContext.create(this);
+
+        requestContext.registerListener(new AuthorizeListener() {
+
+            /* Authorization was completed successfully. */
+            @Override
+            public void onSuccess(AuthorizeResult result) {
+        /* Your app is now authorized for the requested scopes */
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // At this point we know the authorization completed, so remove the ability to return to the app to sign-in again
+//                        setLoggingInState(true);
+                    }
+                });
+                fetchUserProfile();
+            }
+
+            /* There was an error during the attempt to authorize the
+               application. */
+            @Override
+            public void onError(AuthError ae) {
+        /* Inform the user of the error */
+            }
+
+            /* Authorization was cancelled before it could be completed. */
+            @Override
+            public void onCancel(AuthCancellation cancellation) {
+        /* Reset the UI to a ready-to-login state */
+            }
+        });
+
         setContentView(R.layout.activity_main);
+
+        // 给 Login 按钮注册点击事件
+        mLoginButton = findViewById(R.id.login_with_amazon);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthorizationManager.authorize(
+                        new AuthorizeRequest.Builder(requestContext)
+                                .addScopes(ProfileScope.profile(), ProfileScope.postalCode())
+                                .build()
+                );
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 如果 Android　应用的生命周期管理把这个活动关闭了，在 onResume() 方法里把 requestContext 恢复起来。
+        requestContext.onResume();
     }
 }
