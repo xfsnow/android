9aa70822497ce4f32d400d78cf963811ae790195
xfsnow <xuefeng@snowpeak.org>
2017-09-15 15:31:52 +0800

    Completed the whole process of Login with Amazon, with onStart and Logout

--- CognitoAmazon/app/src/main/java/org/snowpeak/cognitoamazon/MainActivity.java
+++ CognitoAmazon/app/src/main/java/org/snowpeak/cognitoamazon/MainActivity.java
@@ -2,8 +2,11 @@ package org.snowpeak.cognitoamazon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.amazon.identity.auth.device.AuthError;
 import com.amazon.identity.auth.device.api.Listener;
@@ -13,14 +16,21 @@ import com.amazon.identity.auth.device.api.authorization.AuthorizeListener;
 import com.amazon.identity.auth.device.api.authorization.AuthorizeRequest;
 import com.amazon.identity.auth.device.api.authorization.AuthorizeResult;
 import com.amazon.identity.auth.device.api.authorization.ProfileScope;
+import com.amazon.identity.auth.device.api.authorization.Scope;
 import com.amazon.identity.auth.device.api.authorization.User;
 import com.amazon.identity.auth.device.api.workflow.RequestContext;
+import com.amazon.identity.auth.device.authorization.api.AmazonAuthorizationManager;
+import com.amazon.identity.auth.device.shared.APIListener;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
 
     private RequestContext requestContext;
+    private AmazonAuthorizationManager mAuthManager;
+    private TextView mProfileText;
     private View mLoginButton;
+    private View mLogoutButton;
+    public String accessToken;
 
     private void fetchUserProfile() {
         User.fetch(this, new Listener<User, AuthError>() {
@@ -53,10 +63,31 @@ public class MainActivity extends AppCompatActivity {
     private void updateProfileData(String name, String email, String account, String zipCode) {
         StringBuilder profileBuilder = new StringBuilder();
         profileBuilder.append(String.format("Welcome, %s!\n", name));
-        profileBuilder.append(String.format("Your email is %s\n", email));
-        profileBuilder.append(String.format("Your zipCode is %s\n", zipCode));
+//        profileBuilder.append(String.format("Your email is %s\n", email));
+//        profileBuilder.append(String.format("Your zipCode is %s\n", zipCode));
         final String profile = profileBuilder.toString();
+        mProfileText.setText(profile);
         Log.d(TAG, "Profile Response: " + profile);
+        setLoggedInState();
+    }
+
+    /**
+     * Sets the state of the application to reflect that the user is currently authorized.
+     */
+    private void setLoggedInState() {
+        mLoginButton.setVisibility(Button.GONE);
+        mLogoutButton.setVisibility(Button.VISIBLE);
+    }
+
+    /**
+     * Sets the state of the application to reflect that the user is not currently authorized.
+     */
+    private void setLoggedOutState() {
+        mLoginButton.setVisibility(Button.VISIBLE);
+        mLogoutButton.setVisibility(Button.GONE);
+//        mIsLoggedIn = false;
+//        setLoggingInState(false);
+        mProfileText.setText(getString(R.string.default_message));
     }
 
     @Override
@@ -96,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        // 给 Login 按钮注册点击事件
+        // 给登录按钮注册点击事件
         mLoginButton = findViewById(R.id.login_with_amazon);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,6 +139,33 @@ public class MainActivity extends AppCompatActivity {
                 );
             }
         });
+        // 显示用户信息的文本块，未登录时显示提示语
+        mProfileText = (TextView) findViewById(R.id.profile_info);
+
+        // 退出按钮，及注册点击事件
+        mLogoutButton = (Button) findViewById(R.id.logout);
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                AuthorizationManager.signOut(getApplicationContext(), new Listener<Void, AuthError>() {
+                    @Override
+                    public void onSuccess(Void response) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                setLoggedOutState();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError(AuthError authError) {
+                        Log.e(TAG, "Error clearing authorization state.", authError);
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -115,5 +173,28 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         // 如果 Android　应用的生命周期管理把这个活动关闭了，在 onResume() 方法里把 requestContext 恢复起来。
         requestContext.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Scope[] scopes = {ProfileScope.profile(), ProfileScope.postalCode()};
+        // 活动开始时调 getToken 检查是否已经登录状态，是登录的直接显示用户信息
+        AuthorizationManager.getToken(this, scopes, new Listener<AuthorizeResult, AuthError>() {
+            @Override
+            public void onSuccess(AuthorizeResult result) {
+                if (result.getAccessToken() != null) {
+                    /* The user is signed in */
+                    fetchUserProfile();
+                } else {
+                    /* The user is not signed in */
+                }
+            }
+
+            @Override
+            public void onError(AuthError ae) {
+                /* The user is not signed in */
+            }
+        });
     }
 }
